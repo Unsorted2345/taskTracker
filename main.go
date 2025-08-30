9420c2288e9b54fc2548a8110d02bb77d2334957
Unsorted2345 <karl.kisterman@gmx.de>
2025-08-30 13:50:34 +0200

    added SQLite database to store work sessions and sessions recorded the timer are now saved in the database

--- main.go
+++ main.go
@@ -1,15 +1,27 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"time"
+
+	_ "modernc.org/sqlite" // Statt go-sqlite3
 )
 
 func main() {
-	timer()
+	// Datenbank öffnen
+	db, err := sql.Open("sqlite", "taskTracker.db")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	fmt.Println("Datenbank geöffnet!")
+	createTable(db)
+	timer(db) // db als Parameter übergeben
 }
 
-func timer() {
+func timer(db *sql.DB) {
 	var (
 		start time.Time
 		end   time.Time
@@ -29,7 +41,51 @@ func timer() {
 		fmt.Scanln(&input)
 		end = time.Now()
 	}
+
 	fmt.Println("timer stopped")
-	elapsed := end.Sub(start).Round(time.Second) // Rundet auf volle Sekunden
-	fmt.Printf("elapsed time: %v\n", elapsed)
+	var title string
+	for title == "" {
+		fmt.Print("Titel eingeben: ")
+		fmt.Scanln(&title)
+		if title == "" {
+			fmt.Println("Titel darf nicht leer sein!")
+		}
+	}
+
+	var description string
+	fmt.Print("Beschreibung (optional): ")
+	fmt.Scanln(&description)
+
+	saveSession(db, title, description, start.Format("2006-01-02 15:04:05"), end.Format("2006-01-02 15:04:05"))
+	defer fmt.Printf("Session '%s' gespeichert! Dauer: %v\n", title, end.Sub(start).Round(time.Second))
+}
+
+// Diese Funktion erstellt die Tabelle (falls sie noch nicht existiert)
+func createTable(db *sql.DB) {
+	query := `
+    CREATE TABLE IF NOT EXISTS work_sessions (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT NOT NULL,
+		description TEXT,
+        start_time TEXT NOT NULL,
+        end_time TEXT
+    );`
+
+	_, err := db.Exec(query)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Tabelle erstellt!")
+}
+
+// Diese Funktion speichert eine Session in der Datenbank
+func saveSession(db *sql.DB, title string, description string, startTime string, endTime string) {
+	query := `INSERT INTO work_sessions (title, description, start_time, end_time) VALUES (?, ?, ?, ?)`
+
+	_, err := db.Exec(query, description, title, startTime, endTime)
+	if err != nil {
+		panic(err)
+	}
+
 }
