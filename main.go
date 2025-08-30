dc3f2ad344d55f589b21133a0070b33bde221e28
Unsorted2345 <karl.kisterman@gmx.de>
2025-08-30 11:29:17 +0200

    timer added

--- main.go
+++ main.go
@@ -2,8 +2,34 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	timer()
+}
+
+func timer() {
+	var (
+		start time.Time
+		end   time.Time
+		input string
+	)
+
+	fmt.Println("type start to start the timer")
+
+	for input != "start" {
+		fmt.Scanln(&input)
+		start = time.Now()
+
+	}
+
+	fmt.Println("timer started, type stop to stop the timer")
+	for input != "stop" {
+		fmt.Scanln(&input)
+		end = time.Now()
+	}
+	fmt.Println("timer stopped")
+	elapsed := end.Sub(start).Round(time.Second) // Rundet auf volle Sekunden
+	fmt.Printf("elapsed time: %v\n", elapsed)
 }
